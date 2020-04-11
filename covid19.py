0e94f935f6c841c61fb453b210fe975734d92205
Ryan Lence <56052994+lenceai@users.noreply.github.com>
2020-04-10 22:02:02 -0700

    20-04-10

--- covid19.py
+++ covid19.py
@@ -18,6 +18,9 @@ df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master
 wa = df[df.state == 'Washington']
 wa = wa[wa.date >= '2020-03-01']
 
+ut = df[df.state == 'Utah']
+ut = ut[ut.date >= '2020-03-01']
+
 # focus on US
 
 us = df[df.date >= '2020-03-01']
@@ -25,6 +28,7 @@ us = df[df.date >= '2020-03-01']
 
 del wa['fips']
 del us['fips']
+del ut['fips']
 
 #%% add the last day's results
 
@@ -44,19 +48,34 @@ us['daily_cases'] = us.cases - us.last_cases
 us['daily_deaths'] = us.deaths - us.last_deaths
 us = us.reset_index()
 
+ut = ut.groupby('date').sum()
+ut_last = ut.shift(periods=1)
+ut['last_cases'] = ut_last.cases
+ut['last_deaths'] = ut_last.deaths
+ut['daily_cases'] = ut.cases - ut.last_cases
+ut['daily_deaths'] = ut.deaths - ut.last_deaths
+ut = ut.reset_index()
+
+
 
 #%% Report on the data
 
-wa.plot(kind='bar', x='date', y='daily_cases')
+wa.daily_cases.plot(kind='bar', label='Washington')
 plt.show()
 
 us.daily_cases.plot(kind='bar')
+plt.show()
+
+ut.daily_cases.plot(kind='bar')
 plt.show()
 
 wa.daily_deaths.plot(kind='bar')
 plt.show()
 
 us.daily_deaths.plot(kind='bar')
+plt.show()
+
+ut.daily_deaths.plot(kind='bar')
 plt.show()
 
 #%%
